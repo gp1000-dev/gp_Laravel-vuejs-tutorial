3cedc4d40641e89171a63212360c91aee9c786ec
eguchi-gp1000 <eguchi@gp1000.jp>
2022-06-21 12:15:08 +0900

    【Vueフロントエンド編】ベースルーティングを追加する
    初回アクセス時にすべてのURLで  app.blade.php へアクセスするように設定しています。

--- src/LaravelVueTaskList/routes/web.php
+++ src/LaravelVueTaskList/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* すべてのURLで app.blade.php を表示する設定 */
+// [/{any}] "/"以下のアクセスを すべて（{any}）に指定する
+// [->where('any', '.*')] 正規表現で"any"に".*（すべて）"を指定する ※where()は複数のルートを指定する際に用いられます
+// "/{any}"の指定のみだと not Found のエラーになる点に注意してください
+Route::get('/{any}', function() {
+    // app.blade.phpを表示する
+    return view('app');
+})->where('any', '.*');
